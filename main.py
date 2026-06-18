01c030ed480a081ead88edb10d345e3bdb170f3b
ecgetheow <theowolens@proofschool.org>
2026-06-18 17:15:13 -0600

    stuff

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import sys
 import tempfile
 import tomllib
 from urllib.parse import urlencode
+from email.message import EmailMessage
+import smtplib
 
 import requests
 from flask import Flask, Response, jsonify, redirect, render_template, request, session, url_for
@@ -1392,6 +1394,45 @@ def courses(course):
         return render_template('course1.html', user=session.get('user'), course_name = course, courses = COURSES[course])
     else:
         return render_template('courses.html', user=session.get('user'))
+
+
+@app.get('/contact-us')
+def contact_us():
+    return render_template('contact_us.html')
+
+@app.get('/feedback')
+def do_feedback():
+    rating = request.args.get('rating')
+    comments = request.args.get('comments')
+
+
+    # 1. Define credentials and addresses
+    SMTP_SERVER = "smtp.gmail.com"  # Replace with your provider's SMTP server
+    SMTP_PORT = 587
+    SENDER_EMAIL = "info.project1716@gmail.com"
+    SENDER_PASSWORD = "wnmbuqjxwngzrgec"  # Do not use your main login password!
+    RECEIVER_EMAIL = "info.project1716@gmail.com"
+
+    # 2. Construct the email message
+    msg = EmailMessage()
+    msg["Subject"] = f"Automated Email from Python"
+    msg["From"] = SENDER_EMAIL
+    msg["To"] = RECEIVER_EMAIL
+    msg.set_content(f"Rating: {rating}, Comments: {comments}")
+
+    # 3. Connect to the server and send
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(SENDER_EMAIL, SENDER_PASSWORD)
+            server.send_message(msg)
+        print("Email sent successfully!")
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+    return render_template('index.html')
+
+
+
 
 
 
